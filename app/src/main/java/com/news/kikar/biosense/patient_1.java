75389951c875b7b64e6ce5b6aa27b2f8bbad9b32
kamish <kamish@himacslkamish2.mynet>
2016-06-20 21:06:42 +0300

    http req

--- app/src/main/java/com/news/kikar/biosense/patient_1.java
+++ app/src/main/java/com/news/kikar/biosense/patient_1.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class patient_1 extends Activity {
 
@@ -20,6 +21,62 @@ public class patient_1 extends Activity {
                 animatedStartActivity();
             }
         });
+
+        Button saveme=(Button)findViewById(R.id.save);
+
+
+        saveme.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v)
+            {
+                //ALERT MESSAGE
+                Toast.makeText(getBaseContext(), "Please wait, connecting to server.", Toast.LENGTH_LONG).show();
+
+                try{
+
+                    // URLEncode user defined data
+
+                    String loginValue    = URLEncoder.encode(login.getText().toString(), "UTF-8");
+                    String fnameValue  = URLEncoder.encode(fname.getText().toString(), "UTF-8");
+                    String emailValue   = URLEncoder.encode(email.getText().toString(), "UTF-8");
+                    String passValue    = URLEncoder.encode(pass.getText().toString(), "UTF-8");
+
+                    // Create http cliient object to send request to server
+
+                    HttpClient Client = new DefaultHttpClient();
+
+                    // Create URL string
+
+                    String URL = "http://androidexample.com/media/webservice/httpget.php?user="+loginValue+"&name="+fnameValue+"&email="+emailValue+"&pass="+passValue;
+
+                    //Log.i("httpget", URL);
+
+                    try
+                    {
+                        String SetServerString = "";
+
+                        // Create Request to server and get response
+
+                        HttpGet httpget = new HttpGet(URL);
+                        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                        SetServerString = Client.execute(httpget, responseHandler);
+
+                        // Show response on activity
+
+                        content.setText(SetServerString);
+                    }
+                    catch(Exception ex)
+                    {
+                        content.setText("Fail!");
+                    }
+                }
+                catch(UnsupportedEncodingException ex)
+                {
+                    content.setText("Fail");
+                }
+            }
+        });
+    }
+
     }
 
     private void animatedStartActivity() {
@@ -28,6 +85,9 @@ public class patient_1 extends Activity {
         final Intent intent = new Intent(getApplicationContext(), patient2.class);
         // disable default animation for new intent
         startActivity(intent);
+
+
+
     }
 }
 
